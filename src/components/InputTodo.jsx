c930907342dc91b0188bcf2d9c086a8ac26990a5
ma-8g <aprgxy@gmail.com>
2025-10-23 08:46:34 +0900

    62_スタイルシートをコンポーネントに移動

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,3 +1,12 @@
+const style = {
+  backgroundColor: "#c6e5d9",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px",
+}
+
 export const InputTodo = (props) => {
     const {todoText, onChange, onClick} = props;
     return (
