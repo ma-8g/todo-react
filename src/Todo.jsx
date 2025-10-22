b428c80d770c159a4296fdaf98535328657bae21
ma-8g <aprgxy@gmail.com>
2025-10-23 07:24:50 +0900

    60_戻す機能追加

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -25,6 +25,14 @@ export const Todo = () => {
     setIncompleteTodos(newIncompleteTodos);
     setCompleteTodos(newCompleteTodos);
   }
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  }
   return (
     <>
       <div className ="input-area">
@@ -48,11 +56,11 @@ export const Todo = () => {
       <div className="complete-area">
         <p className="title">完了のTODO</p>
           <ul>
-            {completeTodos.map((todo) => (
+            {completeTodos.map((todo ,index) => (
               <li key={todo}>
                 <div className="list-row">
                   <p className='todo-item'>{todo}</p>
-                  <button>戻す</button>
+                  <button onClick={() => onClickBack(index)}>戻す</button>
                 </div>
               </li>
             ))}
